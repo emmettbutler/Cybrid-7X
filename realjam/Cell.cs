5350f9fbfb01aad5359280fe77d1fbf8fe9ad950
Nina <ninamarie315@gmail.com>
2013-02-10 15:06:29 -0500

    Cell is moving a bit

--- realjam/Cell.cs
+++ realjam/Cell.cs
@@ -6,14 +6,25 @@ using Sce.PlayStation.Core.Graphics;
 
 namespace realjam {
   public class Cell:SpriteUV {
-    public Cell() {
+
+    public DateTime borntime;
+    public Vector2 anchor;
+
+    public Cell(Vector2 pos) {
       var texture = new TextureInfo( new Texture2D("/Application/assets/eyebulb.png",false));
       TextureInfo = texture;
       Quad.S = texture.TextureSizef;
+
+      borntime = DateTime.UtcNow;
+      Position = pos;
+      anchor = Position;
+
     }
 
     public virtual void Tick(float dt) {
-      Position = new Vector2(Position.X+1,Position.Y-1);
+      TimeSpan t = DateTime.UtcNow - borntime;
+      float ttime = (float) t.TotalMilliseconds/1000.0f;
+      Position = new Vector2(anchor.X+20*(float)System.Math.Sin(ttime*10),anchor.Y);
     }
   }
 }
--- realjam/SpawnManager.cs
+++ realjam/SpawnManager.cs
@@ -3,25 +3,30 @@ using Sce.PlayStation.Core;
 using Sce.PlayStation.HighLevel.GameEngine2D;
 using Sce.PlayStation.HighLevel.GameEngine2D.Base;
 using Sce.PlayStation.Core.Graphics;
+using System.Collections.Generic;
 
 namespace realjam {
   public class SpawnManager {
 
     public Scene scene {get; set;}
     private Cell sprite;
+    public List<Cell> cells {get; set;}
 
     public SpawnManager(Scene scene) {
-      sprite = new Cell();
+      cells = new List<Cell>();
+      sprite = new Cell(scene.Camera.CalcBounds().Center);
 
       sprite.CenterSprite();
 
-      sprite.Position = scene.Camera.CalcBounds().Center;
-
       scene.AddChild(sprite);
+      cells.Add(sprite);
     }
 
     public void FrameUpdate(float dt){
-      sprite.Tick(dt);
+      foreach (Cell c in cells){
+        c.Tick(dt);
+
+      }
     }
   }
 }
