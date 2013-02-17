4a8fa83397c5b5fbcb7ec510271f536931a45115
emmett9001 <ebutler9@gmail.com>
2013-02-17 13:42:47 -0500

    add game over scene

--- realjam/GameOverScene.cs
+++ realjam/GameOverScene.cs
@@ -10,11 +10,12 @@ namespace realjam {
   public class GameOverScene:Scene {
     public GameOverScene() {
       Camera.SetViewFromViewport();
-
-      SpriteTile overs = Support.TiledSpriteFromFile("/Application/assets/cell_0000000054.png", 1, 1);
-      overs.Quad.S = overs.TextureInfo.TextureSizef/2;
-      overs.Position = new Vector2(Camera.CalcBounds().Max.X/2, Camera.CalcBounds().Max.Y/2);
-      AddChild(overs);
+      
+      var bg = Support.TiledSpriteFromFile("/Application/assets/game_over.png", 1, 1);
+      bg.Position = new Vector2((this.Camera.CalcBounds().Max.X)/2,(Camera.CalcBounds().Max.Y)/2);
+      bg.CenterSprite();
+      bg.VertexZ = 0;
+      this.AddChild(bg,0);
     }
   }
 }
