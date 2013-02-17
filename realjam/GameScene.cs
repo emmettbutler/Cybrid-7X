7ef6b92a60406d21c80cd89f8fa284f9c085e6b6
Nina <ninamarie315@gmail.com>
2013-02-16 21:34:03 -0500

    rainyness

--- realjam/GameScene.cs
+++ realjam/GameScene.cs
@@ -5,6 +5,7 @@ using Sce.PlayStation.Core.Graphics;
 using Sce.PlayStation.Core.Input;
 using Sce.PlayStation.HighLevel.GameEngine2D;
 using Sce.PlayStation.HighLevel.GameEngine2D.Base;
+using System.Collections.Generic;
 
 namespace realjam {
   public class GameScene:Scene {
@@ -13,15 +14,17 @@ namespace realjam {
     public SpawnManager spawnmngr {get; set;}
     public Collider collider {get; set;}
     public Boolean isRaining {get; set;}
+    public List<SpriteTile> rain {get; set;}
 
     public GameScene() {
       Camera.SetViewFromViewport();
 
-      isRaining = false;
+      isRaining = true;
 
       var bg = Support.TiledSpriteFromFile("/Application/assets/Background_Object.png", 1, 1);
       bg.Position = new Vector2((Camera.CalcBounds().Max.X)/2,(Camera.CalcBounds().Max.Y)/2);
       bg.CenterSprite();
+      bg.VertexZ = 0;
       this.AddChild(bg,0);
 
       collider = new Collider();
@@ -36,10 +39,34 @@ namespace realjam {
       fencefront.Position = new Vector2((Camera.CalcBounds().Max.X)/2,(Camera.CalcBounds().Min.Y+50)/2);
       fencefront.CenterSprite();
       fencefront.VertexZ = 1;
-      this.AddChild(fencefront,1);
+      this.AddChild(fencefront,0);
       Console.WriteLine(fencefront.Position);
 
       spawnmngr = new SpawnManager(this,collider);
+
+      if(isRaining){
+        rain = new List<SpriteTile>();
+        var rainsprite = Support.TiledSpriteFromFile("/Application/assets/Rain_Object.png", 9, 1);
+        var raincountX = Camera.CalcBounds().Max.X/rainsprite.Quad.X.X;
+        var raincountY = Camera.CalcBounds().Max.Y/rainsprite.Quad.Y.Y;
+        var currentPos = new Vector2(0, 20);
+        for(var i = 0; i < raincountY; i++){
+          for(int j = 0; j < raincountX; j++){
+            rainsprite = Support.TiledSpriteFromFile("/Application/assets/Rain_Object.png", 9, 1);
+            rainsprite.CenterSprite();
+            rainsprite.Position = currentPos;
+            rainsprite.VertexZ = 1;
+            this.AddChild(rainsprite,1);
+
+            rain.Add(rainsprite);
+            var RainAnimation = new Support.AnimationAction(rainsprite, 9, 1, 1.0f, looping: true);
+            rainsprite.RunAction(RainAnimation);
+
+            currentPos += new Vector2(rainsprite.Quad.X.X, 0);
+          }
+          currentPos = new Vector2(0, currentPos.Y + rainsprite.Quad.Y.Y);
+        }
+      }
 
       var goal = new WinSection(new Vector2(Camera.CalcBounds().Max.X-100,Camera.CalcBounds().Max.Y-100),
                                 spawnmngr,this);
