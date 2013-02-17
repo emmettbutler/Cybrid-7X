c86a34766172b8ad123e9eafb4e1654ab25c62f1
Nina <ninamarie315@gmail.com>
2013-02-17 12:38:09 -0500

    fix compile error in intro scene

--- realjam/HelloScene.cs
+++ realjam/HelloScene.cs
@@ -1,10 +1,17 @@
 using System;
+using Sce.PlayStation.Core;
+using Sce.PlayStation.Core.Environment;
+using Sce.PlayStation.Core.Graphics;
+using Sce.PlayStation.Core.Input;
+using Sce.PlayStation.HighLevel.GameEngine2D;
+using Sce.PlayStation.HighLevel.GameEngine2D.Base;
+using System.Collections.Generic;
 
 namespace realjam {
-  public class HelloScene {
+  public class HelloScene:Scene {
     public HelloScene() {
       var bg = Support.TiledSpriteFromFile("/Application/assets/titleScreen.png", 1, 1);
-      bg.Position = new Vector2((Camera.CalcBounds().Max.X)/2,(Camera.CalcBounds().Max.Y)/2);
+      bg.Position = new Vector2((this.Camera.CalcBounds().Max.X)/2,(Camera.CalcBounds().Max.Y)/2);
       bg.CenterSprite();
       bg.VertexZ = 0;
       this.AddChild(bg,0);
